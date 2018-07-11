d8b73082509e47624346121b7d4f129dc25226ee
Alex Babenko <bob78alex@gmail.com>
2018-07-11 02:39:11 +0000

    Adding Stocks and UserStock

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  #root 'welcome#index'
-  #get 'my_portfolio', to: 'users#my_portfolio'
-  root 'users#my_portfolio'
+  
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  #root 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
+  resources :user_stocks, only: [:create]
+  
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
