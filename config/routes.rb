97bab59df8daf2dcddde598377839707aa347b49
Alex Babenko <bob78alex@gmail.com>
2018-07-18 00:43:36 +0000

    Adding friendships functionality, search actions and error handling

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
   resources :users, only: [:show]
   resources :friendships
   resources :user_stocks, only: [:create, :destroy]
